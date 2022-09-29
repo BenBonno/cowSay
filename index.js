851ce7f49ecb466467e32e7df25a70a366efe780
BenBonno <benoit.bonnoron@gmail.com>
2022-09-29 15:07:56 +0200

    init of console cow

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const object = require(".information")
 const cowsay = require("cowsay");
 
-
+console.log(cowsay.say({
+    text: ì'm working in ${object.campusName}`},
+    e : "oo",
+    T: "U "
+}));
